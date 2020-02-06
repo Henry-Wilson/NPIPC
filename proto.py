901c7bd8f06b619d37b097f08f92b60878bf80e6
Henry Wilson <henry.wilson.17@cnu.edu>
2020-02-05 23:47:24 -0500

    comented software

--- proto.py
+++ proto.py
@@ -3,6 +3,7 @@
 
 #By Henry R. Wilson
 
+#Returns e3 as appropriate sum of e1, e2
 def add(e1, e2):
     v1, d1 = e1.eget()
     v2, d2 = e2.eget()
@@ -13,6 +14,7 @@ def add(e1, e2):
     ret = [e3]
     return ret
 
+#Returns e3 as diff of e1, e2
 def sub(e1, e2):
     v1, d1 = e1.eget()
     v2, d2 = e2.eget()
@@ -23,6 +25,7 @@ def sub(e1, e2):
     ret = [e3]
     return ret
 
+#Returns e3 as product of e1, e2
 def mul(e1, e2):
     v1, d1 = e1.eget()
     v2, d2 = e2.eget()
@@ -33,6 +36,7 @@ def mul(e1, e2):
     ret = [e3]
     return ret
 
+#Returns e3 as quotient of e1, e2
 def div(e1, e2):
     v1, d1 = e1.eget()
     v2, d2 = e2.eget()
@@ -43,22 +47,29 @@ def div(e1, e2):
     ret = [e3]
     return ret
 
+#Prints both then returns both
 def two(e1, e2):
     print("E2:",e2,"\nE1:",e1)
     return [e2,e1]
 
+#Stub function.
+#This is not implementable in accordance
+#with the design principle of pop 2, push N.
 def pall(e1, e2):
     print("Not implemented!")
     print("Not possible to implement consistently with design principles.")
     return [e2, e1]
 
+#Simply drop e1
 def die(e1, e2):
     return [e2]
 
+#Push both back, e1 BEFORE e2.
 def swp(e1, e2):
     return [e1, e2]
 
-#              0     1     2     3     4     5     6     7
+#These NEED to stay in the same order as each other.
+#              0     1     2     3     4     5     6
 opstrings = ('add','sub','mul','div','two','die','swp')
 operators = ( add,  sub,  mul,  div,  two,  die,  swp )
 #two and all print so many entries
@@ -71,9 +82,9 @@ class entry:
         self.value = 0.0
         self.delta = 0.0
     #Can be set
-    def eset(self,a,b):
-        self.value = a
-        self.delta = b
+    def eset(self,v,d):
+        self.value = v
+        self.delta = d
     #Can be got
     def eget(self):
         return (self.value, self.delta)
@@ -88,8 +99,11 @@ if __name__ == "__main__":
     stack = [] #Use append and pop to modify
     exit = False
     while(not exit):
+        #Give prompt.
         try:
             command = input(">>")
+        #Let the user know they should exit
+        #by typing "exit" or "quit"
         except:
             print("Rude exit!")
             break;
@@ -97,17 +111,20 @@ if __name__ == "__main__":
         #Operator execution
         if command in opstrings:
             print("Executing", command+"...")
+            #Make sure that the stack can have 2 entries popped at all.
             if len(stack) < 2:
                 print("Insufficient stack size for operation!")
                 continue
             else:
+                #Determine actual operator.
                 i = opstrings.index(command)
                 op = operators[i]
+                #Pop two elements.
                 e1 = stack.pop()
                 e2 = stack.pop()
-                #Execute
+                #Execute op.
                 etuple = op(e1,e2)
-                #This seems to get the right order every time. Does it?
+                #Push everything in the returned list back onto stack
                 for e in etuple:
                     stack.append(e)
 
@@ -115,9 +132,11 @@ if __name__ == "__main__":
         elif command == "quit" or command == "exit":
             exit = True
             continue
+        #Help message
         elif command == "help":
             print("\tNPIPCP Operates on a stack system.")
             print("\tEnter operands in value, uncertainty pairs.")
+            print("\tFor example: 2.33,4.2e-2")
             print("\tCommands:", opstrings)
             print("\tEach command pops two operands.")
             print("\tEach command pushes N operands back.")
@@ -127,15 +146,20 @@ if __name__ == "__main__":
             print("\tswp: pushes both back in opposite order.")
 
         #Value, delta entry
+        #This is the only way to populate the stack
         else:
+            #Note: this works as long as two proper floats
+            #are separated by a comma. Whitespace does not affect things.
             try:
+                #Parse value and delta out
                 value, delta = command.split(',')
                 value = float(value)
                 delta = float(delta)
+                #Buld element
                 e = entry()
                 e.eset(value,delta)
+                #Push
                 stack.append(e)
-                #print("Added to stack!")
             except:
                 print("Malformed command!")
     print("Goodbye!")
